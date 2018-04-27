d6d82acf65c734370c650ea8734815cc57a76c5f
Brian Clark <brian@clark.zone>
2018-04-27 07:58:39 -0400

    Rules to allow inbound from self for gossip/RPC

--- modules/consul-client-security-group-rules/main.tf
+++ modules/consul-client-security-group-rules/main.tf
@@ -24,6 +24,26 @@ resource "aws_security_group_rule" "allow_serf_lan_udp_inbound" {
   security_group_id = "${var.security_group_id}"
 }
 
+resource "aws_security_group_rule" "allow_serf_lan_tcp_inbound_self" {
+  type        = "ingress"
+  from_port   = "${var.serf_lan_port}"
+  to_port     = "${var.serf_lan_port}"
+  protocol    = "tcp"
+  self        = true
+
+  security_group_id = "${var.security_group_id}"
+}
+
+resource "aws_security_group_rule" "allow_serf_lan_udp_inbound_self" {
+  type        = "ingress"
+  from_port   = "${var.serf_lan_port}"
+  to_port     = "${var.serf_lan_port}"
+  protocol    = "udp"
+  self        = true
+
+  security_group_id = "${var.security_group_id}"
+}
+
 resource "aws_security_group_rule" "allow_serf_lan_tcp_inbound_from_security_group_ids" {
   count                    = "${var.allowed_inbound_security_group_count}"
   type                     = "ingress"
--- modules/consul-security-group-rules/main.tf
+++ modules/consul-security-group-rules/main.tf
@@ -79,6 +79,46 @@ resource "aws_security_group_rule" "allow_dns_udp_inbound" {
   security_group_id = "${var.security_group_id}"
 }
 
+resource "aws_security_group_rule" "allow_server_rpc_inbound_self" {
+  type        = "ingress"
+  from_port   = "${var.server_rpc_port}"
+  to_port     = "${var.server_rpc_port}"
+  protocol    = "tcp"
+  self        = true
+
+  security_group_id = "${var.security_group_id}"
+}
+
+resource "aws_security_group_rule" "allow_cli_rpc_inbound_self" {
+  type        = "ingress"
+  from_port   = "${var.cli_rpc_port}"
+  to_port     = "${var.cli_rpc_port}"
+  protocol    = "tcp"
+  self        = true
+
+  security_group_id = "${var.security_group_id}"
+}
+
+resource "aws_security_group_rule" "allow_serf_wan_tcp_inbound_self" {
+  type        = "ingress"
+  from_port   = "${var.serf_wan_port}"
+  to_port     = "${var.serf_wan_port}"
+  protocol    = "tcp"
+  self        = true
+
+  security_group_id = "${var.security_group_id}"
+}
+
+resource "aws_security_group_rule" "allow_serf_wan_udp_inbound_self" {
+  type        = "ingress"
+  from_port   = "${var.serf_wan_port}"
+  to_port     = "${var.serf_wan_port}"
+  protocol    = "udp"
+  self        = true
+
+  security_group_id = "${var.security_group_id}"
+}
+
 resource "aws_security_group_rule" "allow_server_rpc_inbound_from_security_group_ids" {
   count                    = "${var.allowed_inbound_security_group_count}"
   type                     = "ingress"
